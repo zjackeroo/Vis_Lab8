c612c542168766149307c1f9eb696834ea54d7d9
Zjackeroo <jeffery_guo@hotmail.com>
2020-11-13 17:52:53 -0500

    partial label

--- js/script.js
+++ js/script.js
@@ -20,37 +20,43 @@ Promise.all([ // load multiple files
         .domain(d3.extent(driving, d=>d.gas)).nice()
         .range([height-margin.bottom, margin.top]);
 
+    line = d3.line()
+        .curve(d3.curveCatmullRom)
+        .x(d => xScale(d.miles))
+        .y(d => yScale(d.gas));
+    const l = length(line(driving));
+
     xAxis = g => g
         .attr("transform", `translate(0,${height - margin.bottom})`)
         .call(d3.axisBottom(xScale).ticks(width / 80))
-        // .call(g => g.select(".domain").remove())
-        // .call(g => g.selectAll(".tick line").clone()
-        //     .attr("y2", -height)
-        //     .attr("stroke-opacity", 0.1))
-        // .call(g => g.append("text")
-        //     .attr("x", width - 4)
-        //     .attr("y", -4)
-        //     .attr("font-weight", "bold")
-        //     .attr("text-anchor", "end")
-        //     .attr("fill", "black")
-        //     .text(driving.miles)
-        //     .call(halo)
-        // );
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick line").clone()
+            .attr("y2", -height)
+            .attr("stroke-opacity", 0.1))
+        .call(g => g.append("text")
+            .attr("x", width - 4)
+            .attr("y", -4)
+            .attr("font-weight", "bold")
+            .attr("text-anchor", "end")
+            .attr("fill", "black")
+            .text("Mileage")
+            .call(halo)
+        );
     yAxis = g => g
         .attr("transform", `translate(${margin.left},0)`)
         .call(d3.axisLeft(yScale).ticks(null, "$.2f"))
-        // .call(g => g.select(".domain").remove())
-        // .call(g => g.selectAll(".tick line").clone()
-        //     .attr("x2", width)
-        //     .attr("stroke-opacity", 0.1))
-        // .call(g => g.select(".tick:last-of-type text").clone()
-        //     .attr("x", 4)
-        //     .attr("text-anchor", "start")
-        //     .attr("font-weight", "bold")
-        //     .attr("fill", "black")
-        //     .text(driving.gas)
-        //     .call(halo)
-        // );
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick line").clone()
+            .attr("x2", width)
+            .attr("stroke-opacity", 0.1))
+        .call(g => g.select(".tick:last-of-type text").clone()
+            .attr("x", 4)
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .attr("fill", "black")
+            .text("Cost per gallon")
+            .call(halo)
+        );
     
     svg.append("g")
         .call(xAxis);
@@ -67,4 +73,67 @@ Promise.all([ // load multiple files
         .attr("cx", d => xScale(d.miles))
         .attr("cy", d => yScale(d.gas))
         .attr("r", 3);
+
+    const label = svg.selectAll("text")
+        .data(driving)
+        .join("g")
+        .append("text")
+        .text(d=>d.year)
+        .attr("font-size", 10)
+        .attr("x", d=>xScale(d.miles))
+        .attr("y", d=>yScale(d.gas))
+        .each(position)
+        .call(halo);  
+
+    svg.append("path")
+        .datum(driving)
+        .attr("fill", "none")
+        .attr("stroke", "black")
+        .attr("stroke-width", 2)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-dasharray", `0,${l}`)
+        .attr("d", line)
+        .transition()
+        .duration(4000)
+        .ease(d3.easeLinear)
+        .attr("stroke-dasharray", `${l},${l}`);
+
+    function position(d) {
+        const t = d3.select(this);
+        switch (d.side) {
+            case "top":
+            t.attr("text-anchor", "middle").attr("dy", "-0.7em");
+            break;
+            case "right":
+            t.attr("dx", "0.5em")
+                .attr("dy", "0.32em")
+                .attr("text-anchor", "start");
+            break;
+            case "bottom":
+            t.attr("text-anchor", "middle").attr("dy", "1.4em");
+            break;
+            case "left":
+            t.attr("dx", "-0.5em")
+                .attr("dy", "0.32em")
+                .attr("text-anchor", "end");
+            break;
+        }
+    }
+
+    function halo(text) {
+        text
+            .select(function() {
+                return this.parentNode.insertBefore(this.cloneNode(true), this);
+            })
+            .attr("fill", "none")
+            .attr("stroke", "white")
+            .attr("stroke-width", 4)
+            .attr("stroke-linejoin", "round");
+    }
+
+    function length(path) {
+        return d3.create("svg:path").attr("d", path).node().getTotalLength();
+    }
+
 })
